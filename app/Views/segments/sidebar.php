c11252b571137e149be000fcc5e72264336cf9fe
adimiuprix <miuprix@gmail.com>
2024-09-18 12:07:00 +0700

    link sosmed

--- app/Views/segments/sidebar.php
+++ app/Views/segments/sidebar.php
@@ -182,7 +182,7 @@
                 </ul>
             </li> -->
             <li class="menu">
-                <a target="_blank" href="https://t.me/augom" aria-expanded="false" class="dropdown-toggle">
+                <a target="_blank" href="https://t.me/event0Pay" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="icon" data-v-11baff7a=""><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
                         <span>Telegram Group</span>
@@ -190,7 +190,7 @@
                 </a>
             </li>
             <li class="menu">
-                <a target="_blank" href="https://t.me/augom" aria-expanded="false" class="dropdown-toggle">
+                <a target="_blank" href="https://t.me/Gledombop" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="icon" data-v-11baff7a="">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
